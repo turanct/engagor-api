766abd7c49ea7f6d384446da6d1b0b8709da9531
turanct <spinnewebber_toon@hotmail.com>
2019-10-07 21:37:17 +0200

    Add /me api methods to the client class

--- src/Client.php
+++ src/Client.php
@@ -41,11 +41,123 @@ final class Client
         return $decodedResponse;
     }
 
+    /**
+     * Returns details about the currently logged in user.
+     * Use this function to identify who authorized your application.
+     *
+     * https://developers.engagor.com/documentation/endpoints/?url=%2Fme
+     *
+     * @throws ApiCallFailed when something went wrong
+     *
+     * @return array A single user item
+     */
     public function me()
     {
         $request = $this->requestFactory->createRequest(
             'GET',
             'https://api.engagor.com/me/'
+        );
+
+        return $this->execute($request);
+    }
+
+    /**
+     * Returns a list of accounts (and associated projects,
+     * topics and monitored profiles) the logged in user has access to.
+     *
+     * https://developers.engagor.com/documentation/endpoints/?url=%2Fme%2Faccounts
+     *
+     * @param string $pageToken Paging parameter
+     * @param int $limit Amount of accounts to return
+     *
+     * @throws ApiCallFailed when something went wrong
+     *
+     * @return array paged_list of account items
+     */
+    public function getMyAccounts($pageToken = '', $limit = 10)
+    {
+        $request = $this->requestFactory->createRequest(
+            'GET',
+            'https://api.engagor.com/me/accounts'
+        );
+
+        $params = array();
+
+        if ($pageToken !== '') {
+            $params = array(
+                'page_token' => (string) $pageToken,
+            );
+        }
+        if ($limit !== 10) {
+            $params = array(
+                'limit' => (int) $limit,
+            );
+        }
+
+        if (!empty($params)) {
+            $uri = $request->getUri();
+            $uri = $uri->withQuery(http_build_query($params));
+            $request = $request->withUri($uri);
+        }
+
+        return $this->execute($request);
+    }
+
+    /**
+     * Returns a list of the connected profiles for the authenticated user
+     *
+     * https://developers.engagor.com/documentation/endpoints/?url=%2Fme%2Fconnectedprofiles
+     *
+     * @param string $pageToken Paging parameter
+     * @param int $limit Amount of accounts to return
+     *
+     * @throws ApiCallFailed when something went wrong
+     *
+     * @return paged_list of connectedprofile items
+     */
+    public function getMyConnectedProfiles($pageToken = '', $limit = 10)
+    {
+        $request = $this->requestFactory->createRequest(
+            'GET',
+            'https://api.engagor.com/me/connectedprofiles'
+        );
+
+        $params = array();
+
+        if ($pageToken !== '') {
+            $params = array(
+                'page_token' => (string) $pageToken,
+            );
+        }
+        if ($limit !== 10) {
+            $params = array(
+                'limit' => (int) $limit,
+            );
+        }
+
+        if (!empty($params)) {
+            $uri = $request->getUri();
+            $uri = $uri->withQuery(http_build_query($params));
+            $request = $request->withUri($uri);
+        }
+
+        return $this->execute($request);
+    }
+
+    /**
+     * Returns a list of permissions your application has for the currently logged in user
+     *
+     * https://developers.engagor.com/documentation/endpoints/?url=%2Fme%2Fpermissions
+     *
+     * @throws ApiCallFailed when something went wrong
+     *
+     * @return array A list of permissions
+     */
+    public function getMyPermissions()
+    {
+        $request = $this->requestFactory->createRequest(
+            'GET',
+            'https://api.engagor.com/me/permissions'
         );
 
         return $this->execute($request);
